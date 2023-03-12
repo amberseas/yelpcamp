2a4577e3f0f2f3c334ceb2f74b1f54226015f845
Gintarė Šimkūnienė <gintareba@gmail.com>
2023-03-12 16:10:22 +0200

    add mongo atlas

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ const helmet = require('helmet');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
+const MongoStore = require('connect-mongo');
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -42,9 +45,24 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisisasecret';
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
+});
+
+store.on('error', (e) => {
+  console.log('SESSION STORE ERROR', e);
+});
+
 const sessionConfig = {
+  store,
   name: 'session',
-  secret: 'thisisasecret',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
