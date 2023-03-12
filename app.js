e6cc07e5628a057ee3ee7ef27862934f60bcd8ef
Gintarė Šimkūnienė <gintareba@gmail.com>
2023-03-12 16:15:07 +0200

    add start script

--- app.js
+++ app.js
@@ -157,6 +157,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('Connection open on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Connection open on port ${port}`);
 });
